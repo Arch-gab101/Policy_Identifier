450ed8ecc5d3e83073556595e8d4ecf5fa9ad36f
tkhutswa <tkhutswa@gmail.com>
2025-02-19 23:26:17 +0200

    1st iteration

--- Basic_Policy_Application.py
+++ Basic_Policy_Application.py
@@ -1,4 +1,15 @@
 # replicate current inbound flow policy application
 # As a start we will focus on CEX and Blocked Senders Policy to test detection and reporting.
+# develop a flow from terminal and later advance to GUI
 
 import datetime, time, string
+
+mail_from = input("Enter Your Email")
+
+#Block Sender Policy
+block_sender = {
+    'gmail.com' : 'tkhutswa@test.com'
+}
+
+for i in mail_from.split("@"):
+    print(i)
