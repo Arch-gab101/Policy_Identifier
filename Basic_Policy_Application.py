07704b102311f6790ac2c00a89a0c4400114cb68
tkhutswa <tkhutswa@gmail.com>
2025-02-19 23:55:43 +0200

    commit

--- Basic_Policy_Application.py
+++ Basic_Policy_Application.py
@@ -4,17 +4,17 @@
 
 import datetime, time, string
 
-mail_from = input("Enter Your Email")
+mail_from = input("Enter Your Email\n")
 
 
 #Block Sender Policy
-block_sender = ['gmail.com'] #block everything coming inbound from gmail.com
+block_sender = {'gmail.com': 'Un_Pol_#01'} #block everything coming inbound from gmail.com
 array = []
 
 for mail in mail_from.split("@"):
     array.append(mail)
     
 if array[1] in block_sender:
-    print('mail blocked')
+    print(f'# Mail Rejected due to a Block Sender Policy\n# Policy_ID {block_sender["gmail.com"]}')
 else:
     print('Allowed through')
