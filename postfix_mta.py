e210c3324e89d4650b3883c34fa8db049729c0cb
tkhutswa <tkhutswa@gmail.com>
2025-02-20 00:11:26 +0200

    Commit new changes on MTA

--- postfix_mta.py
+++ postfix_mta.py
@@ -2,5 +2,16 @@
 import smtplib
 import email.message
 from email.message import EmailMessage
+import dotenv
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+host = os.getenv('server')
+port = os.getenv('port')
 
 mail = EmailMessage()
+
+
