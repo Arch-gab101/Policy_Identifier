fd0376611284d91690428eb94cdd71913f144f8e
tkhutswa <tkhutswa@gmail.com>
2025-02-22 16:46:18 +0200

    rspamd addition to mail scanning

--- read_logs.py
+++ read_logs.py
@@ -1,11 +1,33 @@
-import csv
-import os.path
+import json
+import re
 import pathlib
 
+logs_path = pathlib.Path('rspamd_log.txt')
+output_json_file = 'rspamd_logs.json'
 
-logs_path = pathlib.Path('/Users/tkhutswa/Documents/Policy_Identifier/Policy_Identifier/read_logs.py')
+log_pattern = re.compile(
+    r"(?P<timestamp>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}) "  
+    r"#(?P<process_id>\d+)\((?P<process_type>\w+)\) "       
+    r"<(?P<task_id>[a-z0-9]+)>; "                          
+    r"(?P<category>\w+); "                                 
+)
+log_array = []
 
 with open(logs_path, 'r') as log:
-    data = log.read()
+    for line in log:
+        match = log_pattern.match(line)
+        if match:
+            log_data = match.groupdict()
+            log_array.append(log_data)
+            print(log_array)
     
-    print(data)
+
+with open(output_json_file, 'w') as rspamd_json:
+     json.dump(log_array, rspamd_json, indent=4)
+     
+print({output_json_file})     
+    
+    
+
+    
+
